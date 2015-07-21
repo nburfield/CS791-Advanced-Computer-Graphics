c0c8d68bd786b99f9eef36df65f2814809f0c06a
Nolan Burfield <nburf@nevada.unr.edu>
2015-07-20 20:38:17 -0700

    shader file needed additional include for linux

--- Tutorial/include/shader.h
+++ Tutorial/include/shader.h
@@ -4,9 +4,13 @@
 #ifndef SHADER_H
 #define SHADER_H
 
+#define GL_DO_NOT_WARN_IF_MULTI_GL_VERSION_HEADERS_INCLUDED
+
 #if defined(__APPLE__) || defined(MACOSX)
+  #include <OpenGL/gl3.h>
   #include <OpenGL/GLU.h>
 #else //linux as default
+  #include <GL/glew.h>
   #include <GL/glu.h>
 #endif
 
